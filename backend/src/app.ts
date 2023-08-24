b333d67047e014053f56905dedbf7b3321c7b31a
RamonHA13 <raan.heam@gmail.com>
2023-08-24 00:08:09 -0600

    Add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express, { json } from 'express'
 import AuthRoutes from './routes/AuthRoutes'
+import ProductsRoutes from './routes/ProductsRoutes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(json())
-app.use(AuthRoutes.Auth, AuthRoutes.router())
+app.use(AuthRoutes.auth, AuthRoutes.router())
+app.use(ProductsRoutes.products, ProductsRoutes.router())
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Hello, world!' })
