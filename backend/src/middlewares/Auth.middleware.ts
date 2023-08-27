bc6e7bc3a74e69c6be99137c280c4c1d020ba38a
RamonHA13 <raan.heam@gmail.com>
2023-08-27 12:45:23 -0600

    Add middlewares

--- backend/src/middlewares/Auth.middleware.ts
+++ backend/src/middlewares/Auth.middleware.ts
@@ -2,6 +2,7 @@ import { type NextFunction, type Request, type Response } from 'express'
 import UserServices from '../helpers/UserServices'
 import StatusMessages from '../helpers/StatusMessages'
 import Jwt from '../helpers/Jwt'
+import { isValidObjectId } from 'mongoose'
 
 export default class AuthMiddlewares {
   public static async isRegistered (req: Request, res: Response, next: NextFunction): Promise<void> {
@@ -23,7 +24,7 @@ export default class AuthMiddlewares {
           next()
           return
         }
-        res.status(404).json({ message: `${StatusMessages.STATUS_401}, user is not registered` })
+        res.status(404).json({ message: `${StatusMessages.STATUS_404}, user is not registered` })
       }
     } catch (error) {
       res.status(500).json({ message: StatusMessages.STATUS_500 })
@@ -31,28 +32,55 @@ export default class AuthMiddlewares {
   }
 
   public static async isAdmin (req: Request, res: Response, next: NextFunction): Promise<void> {
-    /* get role using token desencription getting headers token */
-    const correctToken = req.headers.authorization?.toLocaleLowerCase().startsWith('bearer')
-    const tokenAuth = req.headers.authorization?.split(' ')[1]
+    const token = req.headers.authorization
 
-    if (correctToken === true && tokenAuth !== undefined) {
-      const email = Jwt.checkTokenAndExtractEmail(tokenAuth)
-      try {
-        const user = await UserServices.findByEmail(email, 'role')
-        if (user !== null && user.role === 'admin') {
+    const email = Jwt.checkTokenAndExtractEmail(token)
+    try {
+      const user = await UserServices.findByEmail(email, 'role')
+      if (user !== null) {
+        if (user.role === 'admin') {
           next()
           return
         }
 
-        if (user !== null && user.role === 'client') {
+        if (user.role === 'client') {
           res.status(401).json({ message: `${StatusMessages.STATUS_401}, user doesn't have authorization, should be "Admin" but it's ${user.role}` })
           return
         }
-
-        res.status(401).json({ message: `${StatusMessages.STATUS_401}, user doesn't have authorization` })
-      } catch (error) {
-
       }
+      res.status(404).json({ message: `${StatusMessages.STATUS_404}, User not found` })
+    } catch (error) {
+      res.status(500).json({ message: StatusMessages.STATUS_500 })
     }
+  }
+
+  public static async isValidId (req: Request, res: Response, next: NextFunction): Promise<void> {
+    const { id: bodyId } = req.body
+    const { id: paramsId } = req.params
+
+    if ((bodyId !== undefined && isValidObjectId(bodyId)) || (paramsId !== undefined && isValidObjectId(paramsId))) {
+      next()
+      return
+    }
+
+    res.status(400).json({ message: `${StatusMessages.STATUS_400}, Should be a valid ObjectId` })
+  }
+
+  public static isValidToken (req: Request, res: Response, next: NextFunction): void {
+    const token = req.headers.authorization
+    if (token === undefined) {
+      res.status(400).json({ message: `${StatusMessages.STATUS_400}, Token undefined` })
+      return
+    }
+
+    const isValidToken = token.toLocaleLowerCase().startsWith('bearer')
+    if (isValidToken) {
+      const authToken = token.split(' ')[1]
+      req.headers.authorization = authToken
+      next()
+      return
+    }
+
+    res.status(400).json({ message: `${StatusMessages.STATUS_400}, 'Invalid token format. Use Bearer scheme.` })
   }
 }
--- backend/src/routes/ProductsRoutes.ts
+++ backend/src/routes/ProductsRoutes.ts
@@ -2,24 +2,23 @@ import { Router } from 'express'
 import ProductsControllers from '../controllers/ProductsControllers'
 import Files from '../helpers/Files'
 import AuthMiddlewares from '../middlewares/Auth.middleware'
-// import AuthMiddlewares from '../middlewares/Auth.middleware'
+import FileMiddlewares from '../middlewares/File.middleware'
 
 export default class ProductsRoutes {
   #router = Router()
   public static products = '/products'
-  public static images = '/public/:imageName'
   public static addProduct = '/' // POST
   public static getProducts = '/' // GET
   public static getProduct = '/:id' // GET
-  public static editProduct = '/' // PATCH
+  public static editProduct = '/:id' // PATCH
   public static deleteProduct = '/' // DELETE
 
   constructor () {
-    this.#router.post(ProductsRoutes.addProduct, AuthMiddlewares.isAdmin, new Files('/public/products').multerUpload.single('image'), ProductsControllers.addProduct)
+    this.#router.post(ProductsRoutes.addProduct, AuthMiddlewares.isValidToken, AuthMiddlewares.isAdmin, new Files('/public/products').multerUpload.single('image'), FileMiddlewares.isImage, ProductsControllers.addProduct)
     this.#router.get(ProductsRoutes.getProducts, ProductsControllers.getProducts)
-    this.#router.get(ProductsRoutes.getProduct, ProductsControllers.getProduct)
-    this.#router.patch(ProductsRoutes.editProduct, AuthMiddlewares.isAdmin, new Files('/public/products').multerUpload.single('image'), ProductsControllers.editProduct)
-    this.#router.delete(ProductsRoutes.deleteProduct, AuthMiddlewares.isAdmin, ProductsControllers.deleteProduct)
+    this.#router.get(ProductsRoutes.getProduct, AuthMiddlewares.isValidId, ProductsControllers.getProduct)
+    this.#router.patch(ProductsRoutes.editProduct, AuthMiddlewares.isValidToken, AuthMiddlewares.isValidId, AuthMiddlewares.isAdmin, new Files('/public/products').multerUpload.single('image'), ProductsControllers.editProduct)
+    this.#router.delete(ProductsRoutes.deleteProduct, AuthMiddlewares.isValidToken, AuthMiddlewares.isValidId, AuthMiddlewares.isAdmin, ProductsControllers.deleteProduct)
   }
 
   public static router (): Router {
