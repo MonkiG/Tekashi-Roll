9c9609583a379d7496ca9b1a1b3bed18c183a7eb
RamonHA13 <raan.heam@gmail.com>
2023-08-29 08:21:39 -0600

    Add nodemailer variables

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,7 +5,9 @@ const config = {
   PORT: process.env.PORT as string,
   DATABASE_URL: process.env.DATABASE_URL as string,
   DATABASE_NAME: process.env.NODE_ENV === 'test' ? process.env.DATABASE_TEST_NAME as string : process.env.DATABASE_NAME as string,
-  JWT_SECRET: process.env.JWT_SECRET as string
+  JWT_SECRET: process.env.JWT_SECRET as string,
+  NODEMAILER_AUTH_MAIL: process.env.NODEMAILER_AUTH_MAIL,
+  NODEMAILER_AUTH_PASS: process.env.NODEMAILER_AUTH_PASS
 }
 
 export default config
