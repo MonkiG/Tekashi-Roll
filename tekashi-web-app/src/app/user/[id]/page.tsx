a7eca629b604d9fd8f314eb59318e918604f8632
MonkiG <raan.heam@gmail.com>
2024-03-11 13:15:43 -0600

    Fix user view

--- tekashi-web-app/src/app/user/[id]/page.tsx
+++ tekashi-web-app/src/app/user/[id]/page.tsx
@@ -1,5 +1,40 @@
-export default function UserId ({ params }: { params: { id: string } }): JSX.Element {
+import Footer from '@/app/components/Footer'
+import Header from '@/app/components/Header'
+import { getUserBySession } from '@/app/services/authServices'
+import { getUserRoleBySession } from '@/app/services/userServices'
+import { redirect } from 'next/navigation'
+
+export default async function UserId ({ params }: { params: { id: string } }): Promise<JSX.Element> {
+  const user = await getUserBySession()
+  let userData
+
+  /* hacer un getUserCredentials */
+
+  if (!user) redirect('/')
+  if (user) {
+    const userRole = await getUserRoleBySession(user)
+    userData = user.user_metadata
+    userData.userId = user.id
+    userData.userRole = userRole
+  }
+
   return (
-        <h2>User Id: {params.id}</h2>
+    <>
+      <Header
+      path='/'
+      userData={
+          (userData) &&
+        {
+          userName: `${userData.name} ${userData.lastName}`,
+          userId: userData.id,
+          userRole: userData.userRole
+        }
+      }/>
+        <h2 className='uppercase'>
+          informacion de cuenta
+        </h2>
+
+      <Footer />
+    </>
   )
 }
--- tekashi-web-app/src/app/user/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function User (): JSX.Element {
-  return (
-        <h2>User page</h2>
-  )
-}
